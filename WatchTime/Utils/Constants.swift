2e6b0c56a82e1af67cbe16bde503eb6c6cac18e3
busesahinbas <85345494+busesahinbas@users.noreply.github.com>
2023-04-10 09:44:55 +0300

    [feat] constants and new groups crated

---
+++ WatchTime/Utils/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  WatchTime
+//
+//  Created by Buse Şahinbaş on 9.04.2023.
+//
+
+import Foundation
+
+struct Network {
+    let baseUrl = "https://api.themoviedb.org"
+    let apiKey = "cddca74979cb6b2cd49d2a06b8ec0e2c"
+}
+
