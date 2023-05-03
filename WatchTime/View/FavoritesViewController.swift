97feb600fcd44cfdad321d4a1d72b6789e85c442
busesahinbas <85345494+busesahinbas@users.noreply.github.com>
2023-05-03 20:05:02 +0300

    [fix] favorite view bug fixes

--- WatchTime/View/FavoritesViewController.swift
+++ WatchTime/View/FavoritesViewController.swift
@@ -30,7 +30,7 @@ extension FavoritesViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return  10
+        return  12
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
